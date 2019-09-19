762cff361f19474031d185ace7c21a5244ade756
Ángel García Menéndez <flecktarn121@protonmail.com>
2019-09-19 23:37:13 +0200

    The crawler works, just some refactoring remaining

--- practice2/crawler.py
+++ practice2/crawler.py
@@ -2,12 +2,13 @@ import sys
 import requests
 import time
 from bs4 import BeautifulSoup
+import urllib.parse
 
 urls = []
 visitedPages = {}
 
 urlFile = sys.argv[1]
-maxFiles = sys.argv[2]
+maxFiles = 10
 waitingTime = 10 
 
 def getUrl(filename):
@@ -17,34 +18,47 @@ def getUrl(filename):
         urls.append(line)
     file.close()
 
-def crawl(url, seconds, numberOfLinks):
+def crawl(url, seconds):
+    global maxFiles
+    if url == "":
+        return
     if url in visitedPages:
         return
     else:
         visitedPages[url] = ""
-    if numberOfLinks == 0:
+    if maxFiles < 1:
         return
-    #else:
-        #numberOfLinks -= 1
+    else:
+        maxFiles -= 1
     html = download(url)
-    if len(html) == 0:
+    if html == "":
         return
     soup = BeautifulSoup(html)
-    file = open(soup.title.string+'html', 'w+')
+    print("Saving " + url + "...")
+    print("Remaining pages: "+ str(maxFiles))
+    file = open(soup.title.string.replace("/","|")+'.html', 'w+')
     file.write(html)
     file.close()
     #discard all elements except the ones with the <a> tag
     for link in soup.find_all('a', href = True):
         #get the href attribute of the html tag
-        crawl(link['href'], seconds, numberOfLinks)
-        #TODO: check if the link is a subdomain
+        l = normalizeLink(url, link["href"])
+        crawl(l, seconds) 
+
+def normalizeLink(url, link):
+    if link.startswith("/") or link.startswith("#") or link.startswith("../"):
+        return urllib.parse.urljoin(url, link)
+    else:
+        return link
 
 def download(url):
     request = requests.get(url)
     if "text/html" in request.headers["content-type"]:
+        return request.text
+    else:
         return ""
-    return request.text
 
 getUrl(urlFile)
 for url in urls:
-    crawl(url, waitingTime, maxFiles)
+    crawl(url, waitingTime)
+    maxFiles = 10
