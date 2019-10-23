9e56ffc31607c0f17fa88b552776ed95614bc1d1
Ángel García Menéndez <flecktarn121@protonmail.com>
2019-10-23 10:50:42 +0200

    Docstrings for the inverted index.

--- practice5/InvertedIndex.py
+++ practice5/InvertedIndex.py
@@ -11,36 +11,79 @@ from nltk.stem import WordNetLemmatizer
 
 
 class InvertedIndex:
+    ''' Class representing an inverted index, on which terms are accessed.'''
+
     def __init__(self):
+        ''' No arguments are required for its creation. '''
         self.terms = {}
         self.number_of_documents = 0
 
     def put(self, term, entry):
-        '''Inserts an index entry according to the given term '''
+        ''' Inserts an index entry according to the given term
+            
+            Args:
+                term (str): the term to be inserted.
+                entry (IndexEntry): the content of the entry itself.
+        '''
+
         self.terms[term] = entry 
 
     def get_entry(self, term):
+        '''Given a term, returns the index entry correspoding to it.
+            
+            Args:
+                term (str): the term for which the entry is desired.
+
+            Returns:
+                entry (IndexEntry): the entry corresponding to the term.
+
+            Raises:
+                EntryError: if the trm is not contained in the index
+        '''
         term_in_index(term)
         return self.terms[term]
 
     def update(self, term, document_id, tf):
-        '''Updates the post-list for the entry of a term'''
+        '''Updates the post-list for the entry of a term
+            
+            Args:
+                term (str): '''
         self.terms[term].update_post_list(document_id, tf)
 
-    def contains_term(self, term):
-        return term in self.terms.keys()
-
     def get_idf(self, term):
+        ''' Returns the Inverse Document Frequency for a term
+            
+            Args:
+                term (str): the term for which the IDF is wanted.
+
+            Returns:
+                float: the Inverse Document Frequency of the term.
+
+            Raises:
+                EntryError: if the term is not in the index
+        '''
         self.term_in_index(term)
         return self.terms[term].get_idf()
 
     def get_post_list(self, term):
-        '''Returns the post_list conrresponding to the term given as an argument.'''
+        ''' Returns the post_list conrresponding to the term given as an argument.
+            
+            Args:
+                term (str): the term for which the post-list is desired.
+
+            Returns:
+                dict: a python dictionary with the ids of the documents that contains the term
+                    and the corresponding Term Frequency
+
+            Raises:
+                EntryError: if th term is not in the index
+        '''
         self.term_in_index(term)
         return self.terms[term].post_list
         
 
     def term_in_index(self, term):
+        # Internal method: if the term is not in the index, raise an EntryError.
         if term not in self.terms.keys():
             raise Exception("No entry for term " + term)
 
@@ -49,7 +92,7 @@ class InvertedIndex:
         This method feeds the index with the documents corresponding to the collection.
 
         Args:
-        documents: a python dictionary on which the keys are the document's identifier, 
+        documents (dict): a python dictionary on which the keys are the document's identifier, 
             and the value he string with the document's contents
         '''
         self.number_of_documents = len(documents)
@@ -66,6 +109,7 @@ class InvertedIndex:
 
             
     def string_2_bag_of_words(self, text):
+        #Internal method. Converts a given string to a bag of words
         # Remove punctuation symbols (or simply just consider alphanumeric ones)
         text=text.translate(str.maketrans('', '', string.punctuation))  
         tokens = nltk.word_tokenize(text)
@@ -83,22 +127,44 @@ class InvertedIndex:
         return words
 
 class IndexEntry:
+    ''' Class representing one entry of the Inverted Index.'''
+
     def __init__(self, term, post_list, number_of_documents, idf = 0):
+        ''' The entry should contain the term itself, plus the post_list and the idf.
+            
+            Args:
+                term (str): the term the entry is associated with.
+                post_list (dict): a python dictionary with the ids of the documents
+                    which contain the term, and the corresponding TF.
+                idf (float): the idf for the term, 0 by default.
+        '''
         self.term = term
         self.idf = idf
         self.post_list = post_list
         self.number_of_documents = number_of_documents
 
+    @property
     def get_idf(self):
+        '''float: the current idf for a document.'''
         if self.idf == 0:
             self.update_idf()
         else:
             return self.idf
 
+    @property
+    def get_post_list(self):
+        '''dict: a python dictionary with the id of the documents
+               on which the term is present, and the corresponding
+               Term Frequency.'''
+                    
+        return self.post_list
+
     def update_post_list(self, document_id, tf):
+        # Internal method: add new documents for the entry term
         self.post_list[document_id] = tf
 
     def update_idf(self):
+        # Internal method: update the idf for th entry term
         documents_with_term = len(self.post_list) + 1
         print(self.number_of_documents / documents_with_term)
         self.idf = math.log10(self.number_of_documents / documents_with_term) 
--- practice6/practice6.py
+++ practice6/practice6.py
@@ -1,9 +1,26 @@
+from __future__ import division
+import nltk
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('punkt')
 
 class QueryTokenizer:
 
     def tokenize(self, querie):
-        #TODO: implement the tokenizer
-        return
+        # Remove punctuation symbols (or simply just consider alphanumeric ones)
+        text=text.translate(str.maketrans('', '', string.punctuation))  
+        tokens = nltk.word_tokenize(text)
+        # get rid of stop words
+        words = []
+        lemmatizer = WordNetLemmatizer()
+        for token in tokens:
+            token = lemmatizer.lemmatize(token.lower())
+            if token not in stopwords.words('english'):
+                if token not in words:
+                    words.append(token)
+
+        return words
+
 class QueryManager:
 
     def __init__(self, index):
@@ -12,13 +29,19 @@ class QueryManager:
 
     def process_query(self, query):
         terms = self.tokenizer.tokenize(query)
-        documents = self.retrieve_documents(terms)
-        results = self.calculate_similarity(query, documents)
+        entries = self.retrieve_entries(terms)
+        results = self.calculate_similarity(query, entries)
         return results
 
-    def retrieve_documents(self, terms):
+    def retrieve_entries(self, terms):
         documents = []
         for term in terms:
             document = self.index.get_entry(term)
             documents.append(document)
         return documents
+
+    def calculate_similarity(self, query, entries):
+        results = []
+        for term in query:
+            
+            
